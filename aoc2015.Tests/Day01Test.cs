e20f0476eb83a78c20857c88d32386ab6f178a74
plogen <plogen@gmail.com>
2022-11-05 12:26:20 +0100

    AdventOfCode 2015 starts

---
+++ aoc2015.Tests/Day01Test.cs
@@ -0,0 +1,50 @@
+using Common;
+
+namespace aoc2015.Tests
+{
+
+    [TestFixture]
+    public class TestDay01
+    {
+        private readonly int day = 1;
+        private string input = null!;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            input = ReadInputFile.GetInput(day, "input.txt").First();
+        }
+
+        [Test]
+        [TestCase("(())", 0)]
+        [TestCase("()()", 0)]
+        [TestCase("(((", 3)]
+        [TestCase("(()(()(", 3)]
+        [TestCase("))(((((", 3)]
+        [TestCase("())", -1)]
+        [TestCase("))(", -1)]
+        [TestCase(")))", -3)]
+        [TestCase(")())())", -3)]
+        public void ExamplePart1(string input, int expected)
+        {
+            var answer = Day01.Part1(input);
+            Assert.AreEqual(expected, answer);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var answer = Day01.Part1(input);
+            Assert.AreEqual(-1, answer);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            var answer = Day01.Part2(input);
+            Assert.AreEqual(-1, answer);
+        }
+    }
+
+
+}
---
+++ aoc2015/Day/01/Day01.cs
@@ -0,0 +1,23 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc2015
+{
+    public static class Day01
+    {
+        public static int Part1(string input)
+        {
+            return 0;
+        }
+
+        public static int Part2(string input)
+        {
+            return 0;
+        }
+
+    }
+}
