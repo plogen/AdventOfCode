f01fbdffafc4dcb5b3681d1abff68db6b977a73f
plogen <plogen@gmail.com>
2022-11-09 21:39:47 +0100

    Day10 Part1+2 done

--- aoc2015.Tests/Day10Test.cs
+++ aoc2015.Tests/Day10Test.cs
@@ -15,31 +15,31 @@ namespace aoc2015.Tests
         }
 
         [Test]
-        [TestCase(1, 11)]
-        [TestCase(11, 21)]
-        [TestCase(21, 1211)]
-        [TestCase(1211, 111221)]
-        [TestCase(111221, 312211)]
-        public void ExamplePart1(int input, int expected)
+        [TestCase("1", "11")]
+        [TestCase("11", "21")]
+        [TestCase("21", "1211")]
+        [TestCase("1211", "111221")]
+        [TestCase("111221", "312211")]
+        public void ExamplePart1(string input, string expected)
         {
-            var answer = Day10.Part1(input);
-            Assert.AreEqual(expected, answer);
+            var repeatedCharGroups = Day10.GetRepeatedCharGroups(input);
+            Assert.AreEqual(expected, Day10.LookAndSay(repeatedCharGroups));
         }
 
         [Test]
-        [TestCase(1321131112, 0)]
-        public void Part1(int input, int expected)
+        [TestCase("1321131112", 492982)]
+        public void Part1(string input, int expected)
         {
             var answer = Day10.Part1(input);
-            Assert.AreEqual(0, answer);
+            Assert.AreEqual(expected, answer.Length);
         }
 
         [Test]
-        [TestCase(-1, 0)]
-        public void Part2(int input, int expected)
+        [TestCase("1321131112", 6989950)]
+        public void Part2(string input, int expected)
         {
             var answer = Day10.Part2(input);
-            Assert.AreEqual(0, answer);
+            Assert.AreEqual(expected, answer);
         }
     }
 
--- aoc2015/Day/10/Day10.cs
+++ aoc2015/Day/10/Day10.cs
@@ -12,44 +12,70 @@ using System.Threading.Tasks;
 using static aoc2015.Day03;
 using static aoc2015.Day07;
 using System.Collections;
+using MoreLinq;
 
 namespace aoc2015
 {
     public static class Day10
     {
-        public static int Part1(int input)
+        public static string Part1(string input)
         {
-            var digits = GetDigits(input);
-
-            //var adj = 0;
-            //var t = digits.Zip(digits.Skip(1).Concat(new TimeStatus[] { null }),
-            //        (x, y) => new { x, key = (x == null || y == null || x.Status == y.Status) ? adj : adj++ }
-            //    ).GroupBy(i => i.key, (k, g) => g.Select(e => e.x));
-
-            //var t = digits.GetEnumerator();
-            //while (t.MoveNext())
-            //{
-            //    var current = t.Current;
-            //    t.
-            //}
-            return -1;
-        }
-
-        public static int Part2(int input)
-        {
-            return -1;
-        }
-
-
-        public static IEnumerable<int> GetDigits(int source)
-        {
-            while (source > 0)
+            //Brute force going through each iteration, gets exponentionally more data and processing time as iteration increaeases
+            var data = input;
+            for (int i = 0; i < 40; i++)
             {
-                var digit = source % 10;
-                source /= 10;
-                yield return digit;
+                var repeatedCharGroups = GetRepeatedCharGroups(data);
+                data = LookAndSay(repeatedCharGroups);
             }
+            return data;
         }
+
+        public static List<List<char>> GetRepeatedCharGroups(string input)
+        {
+            var charGroups = new List<List<char>>();
+            var lastHandledChar = '!';
+            foreach (var c in input)
+            {
+                if (lastHandledChar == '!' || c != lastHandledChar)
+                {
+                    charGroups.Add(new List<char>() { c });
+                }
+                else
+                {
+                    charGroups.Last().Add(c);
+                }
+                lastHandledChar = c;
+            }
+            return charGroups;
+        }
+
+        public static string LookAndSay(List<List<char>> repeatedChars)
+        {
+            string lookAndSay = "";
+            foreach (var repeat in repeatedChars)
+            {
+                lookAndSay += $"{repeat.Count}{repeat[0]}";
+            }
+            return lookAndSay;
+        }
+
+
+        public static int Part2(string input)
+        {
+
+            var test = input.Select(c => c - '0').ToArray();
+
+            var result = Enumerable.Range(1, 50)
+                .Aggregate(input.Select(c => c - '0').ToArray(), //Convert chars to int[] and use as TSource
+                   (accumulated, _) => //An accumulator function to be invoked on each element.
+                 accumulated.GroupAdjacent(n => n)
+                .SelectMany(g => new int[] { g.Count(), g.First() })
+                .ToArray())
+                .Count(); //A function to transform the final accumulator value into the result value.
+            return result;
+        }
+
+
 
 
     }
