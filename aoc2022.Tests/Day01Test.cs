271897d37d6c8e48a6dd92825a1bf6bbefb86d4a
Emil Prelog <emil.prelog@veoneer.com>
2022-12-01 12:58:17 +0100

    Template for Day1

---
+++ aoc2022.Tests/Day01Test.cs
@@ -0,0 +1,54 @@
+using Common;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc2022.Tests
+{
+
+    [TestFixture]
+    public class TestDay01
+    {
+        private readonly int day = 1;
+        private List<string> input = null!;
+        private List<string> exampleInput = null!;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            input = ReadInputFile.GetInputDayPadding(day, "input.txt");
+            exampleInput = ReadInputFile.GetInputDayPadding(day, "exampleInput.txt");
+        }
+
+        [Test]
+        public void ExamplePart1()
+        {
+            var answer = Day01.Part1(input);
+            Assert.AreEqual(24000, answer);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var answer = Day01.Part1(input);
+            Assert.AreEqual(-1, answer);
+        }
+
+
+        [Test]
+        public void ExamplePart2()
+        {
+            var answer = Day01.Part2(input);
+            Assert.AreEqual(-1, answer);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            var answer = Day01.Part2(input);
+            Assert.AreEqual(-1, answer);
+        }
+    }
+
+
+}
---
+++ aoc2022/Day/01/Day01.cs
@@ -0,0 +1,25 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc2022
+{
+    public static class Day01
+    {
+        public static int Part1(List<string> input)
+        {
+            return 0;
+        }
+
+        public static int Part2(List<string> input)
+        {
+            return 0;
+        }
+
+
+
+    }
+}
