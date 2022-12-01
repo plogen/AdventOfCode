74923f67eaf1b2636c5740b527f3efb3c2079448
Emil Prelog <emil.prelog@veoneer.com>
2022-12-01 23:22:24 +0100

    Day2 template

---
+++ aoc2022.Tests/Day02Test.cs
@@ -0,0 +1,47 @@
+using Common;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace aoc2022.Tests
+{
+
+    [TestFixture]
+    public class TestDay02
+    {
+        private readonly int day = 2;
+        private List<string> input = null!;
+        private List<string> exampleInput = null!;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            input = ReadInputFile.GetInputDayPadding(day, "input.txt");
+            exampleInput = ReadInputFile.GetInputDayPadding(day, "exampleInput.txt");
+        }
+
+        [Test]
+        public void ExamplePart1()
+        {
+            var answer = Day01.Part1(exampleInput);
+            Assert.AreEqual(-1, answer);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var answer = Day01.Part1(input);
+            Assert.AreEqual(-1, answer);
+        }
+
+
+        [Test]
+        public void Part2()
+        {
+            var answer = Day01.Part2(input);
+            Assert.AreEqual(-1, answer);
+        }
+    }
+
+
+}
---
+++ aoc2022/Day/02/Day02.cs
@@ -0,0 +1,22 @@
+﻿using Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace aoc2022
+{
+    public static class Day02
+    {
+        public static long Part1(List<string> input)
+        {
+            return 0;
+        }
+
+        public static long Part2(List<string> input)
+        {
+            return 0;
+        }
+    }
+}
