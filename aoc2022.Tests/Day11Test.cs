a047b816adecfd15bd560a228612db7b052756d8
plogen <plogen@gmail.com>
2023-11-26 21:29:41 +0100

    Day11 generated with AocCli

---
+++ aoc2022.Tests/Day11Test.cs
@@ -0,0 +1,53 @@
+﻿using Common;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using aoc2022;
+
+namespace aoc2022.Tests
+{
+
+    [TestFixture]
+    public class Day11Test
+    {
+        private const int day = 11;
+        private List<string> input = null!;
+        private List<string> exampleInput = null!;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            input = ReadInputFile.GetInputDayPadding(11, "input.txt");
+            exampleInput = ReadInputFile.GetInputDayPadding(11, "exampleInput.txt");
+        }
+
+        [Test]
+        public void ExamplePart1()
+        {
+            var answer = new Day11().Part1(exampleInput);
+            Assert.AreEqual(10605, answer);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var answer = new Day11().Part1(input);
+            Assert.AreEqual(-1, answer);
+        }
+
+        [Test]
+        public void ExamplePart2()
+        {
+            var answer = new Day11().Part2(exampleInput);
+            Assert.AreEqual(-1, answer);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            var answer = new Day11().Part2(input);
+            Assert.AreEqual(-1, answer);
+        }
+
+    }
+}
---
+++ aoc2022/Day/11/Day11.cs
@@ -0,0 +1,18 @@
+﻿using Common;
+
+//Puzzle: https://adventofcode.com/2022/day/11
+namespace aoc2022
+{
+    public class Day11: DayPuzzle
+    {
+        public override object Part1(List<string> input)
+        {
+            return -1;
+        }
+
+        public override object Part2(List<string> input)
+        {
+            return -1;
+        }
+    }
+}
