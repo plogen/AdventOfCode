951cc6aba6f2979dd75e5776b3abb170580565f6
plogen <plogen@gmail.com>
2023-12-03 21:37:41 +0100

    Working Day02 Part1

---
+++ aoc2023.Tests/Day02Test.cs
@@ -0,0 +1,53 @@
+﻿using Common;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using aoc2023;
+
+namespace aoc2023.Tests
+{
+
+    [TestFixture]
+    public class Day02Test
+    {
+        private const int day = 02;
+        private List<string> input = null!;
+        private List<string> exampleInput = null!;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            input = ReadInputFile.GetInputDayPadding(02, "input.txt");
+            exampleInput = ReadInputFile.GetInputDayPadding(02, "exampleInput.txt");
+        }
+
+        [Test]
+        public void ExamplePart1()
+        {
+            var answer = new Day02().Part1(exampleInput);
+            Assert.AreEqual(8, answer);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var answer = new Day02().Part1(input);
+            Assert.AreEqual(-1, answer);
+        }
+
+        [Test]
+        public void ExamplePart2()
+        {
+            var answer = new Day02().Part2(exampleInput);
+            Assert.AreEqual(-1, answer);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            var answer = new Day02().Part2(input);
+            Assert.AreEqual(-1, answer);
+        }
+
+    }
+}
---
+++ aoc2023/Day/02/Day02.cs
@@ -0,0 +1,58 @@
+﻿using Common;
+using System.Text.RegularExpressions;
+
+//Puzzle: https://adventofcode.com/2023/day/2
+namespace aoc2023
+{
+    public class Day02: DayPuzzle
+    {
+        public static Regex gameIdRegex = new Regex(@"Game (\d+)");
+        public static Regex redRegex = new Regex(@"(\d+)(?: red)");
+        public static Regex greenRegex = new Regex(@"(\d+)(?: green)");
+        public static Regex blueRegex = new Regex(@"(\d+)(?: blue)");
+        public override object Part1(List<string> input)
+        {
+            var refFame = new Game(0, 12, 13, 14);
+            List<Game> games = new();
+            input.ForEach(x => games.Add(GetGame(x)));
+
+            List<Game> gamesThatFit = new();
+            foreach (var game in games)
+            {
+                if (GameFits(refFame, game))
+                    gamesThatFit.Add(game);
+            }
+            
+            return gamesThatFit.Select(g => g.gameId).Sum();
+        }
+
+        public override object Part2(List<string> input)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Game GetGame(string input)
+        {
+             
+            var gameId = gameIdRegex.Matches(input).Select(x => int.Parse(x.Groups[1].Value)).First();
+            var red = redRegex.Matches(input).Select(x => int.Parse(x.Groups[1].Value)).Max();
+            var green = greenRegex.Matches(input).Select(x => int.Parse(x.Groups[1].Value)).Max();
+            var blue = blueRegex.Matches(input).Select(x => int.Parse(x.Groups[1].Value)).Max();
+            return new Game(gameId, red, green, blue);
+        }
+
+        private bool GameFits(Game refGame, Game game)
+        { 
+            if(game.red > refGame.red)
+                return false;
+            if (game.green > refGame.green)
+                return false;
+            if (game.blue > refGame.blue)
+                return false;
+
+            return true; 
+        }
+
+        record Game(int gameId, int red, int green, int blue);
+    }
+}
