214a6bf83fafa81b18d6d0d478aff7ca131516ad
plogen <plogen@gmail.com>
2023-12-03 23:19:00 +0100

    Working Day03 Part1

---
+++ aoc2023.Tests/Day03Test.cs
@@ -0,0 +1,53 @@
+﻿using Common;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using aoc2023;
+
+namespace aoc2023.Tests
+{
+
+    [TestFixture]
+    public class Day03Test
+    {
+        private const int day = 03;
+        private List<string> input = null!;
+        private List<string> exampleInput = null!;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            input = ReadInputFile.GetInputDayPadding(03, "input.txt");
+            exampleInput = ReadInputFile.GetInputDayPadding(03, "exampleInput.txt");
+        }
+
+        [Test]
+        public void ExamplePart1()
+        {
+            var answer = new Day03().Part1(exampleInput);
+            Assert.AreEqual(4361, answer);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var answer = new Day03().Part1(input);
+            Assert.AreEqual(544433, answer);
+        }
+
+        [Test]
+        public void ExamplePart2()
+        {
+            var answer = new Day03().Part2(exampleInput);
+            Assert.AreEqual(-1, answer);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            var answer = new Day03().Part2(input);
+            Assert.AreEqual(-1, answer);
+        }
+
+    }
+}
---
+++ aoc2023/Day/03/Day03.cs
@@ -0,0 +1,87 @@
+﻿using Common;
+using System.Text.RegularExpressions;
+
+//Puzzle: https://adventofcode.com/2023/day/3
+namespace aoc2023
+{
+    public class Day03: DayPuzzle
+    {
+        public static Regex numberRegex = new Regex(@"(\d+)");
+        public static Regex symbolRegex = new Regex(@"[^.0-9\.]");
+        public override object Part1(List<string> input)
+        {
+            List<int> partNumbers = new();
+
+            for (int i = 0; i < input.Count; i++)
+            {
+                var numbers = numberRegex.Matches(input[i]);
+
+                foreach (Match number in numbers)
+                {
+                    List<string> areas = new();
+
+                    string upperArea = null;
+                    if (i > 0)
+                    {
+                        int start = 0;
+                        if (number.Index != 0)
+                            start = number.Index - 1;
+
+                        int end = 0;
+                        if (number.Index + number.Length != input[i].Length)
+                            end = number.Length + (number.Index == 0 ? 1 : 2);
+                        else
+                            end = number.Length;
+
+                        upperArea = input[i - 1].Substring(start, end);
+                        areas.Add(upperArea);
+                    }
+
+
+                    string lowerArea = null;
+                    if ((i + 1) < input.Count)
+                    {
+                        int start = 0;
+                        if (number.Index != 0)
+                            start = number.Index - 1;
+
+                        int end = 0;
+                        if (number.Index + number.Length != input[i].Length)
+                            end = number.Length + (number.Index == 0 ? 1 : 2);
+                        else
+                            end = number.Length;
+
+                        lowerArea = input[i+1].Substring(start, end);
+                        areas.Add(lowerArea);
+                    }
+
+                    string leftArea = null;
+                    if (number.Index > 0)
+                    {
+                        leftArea = input[i].Substring(number.Index-1, 1);
+                        areas.Add(leftArea);
+                    }
+
+                    string rightArea = null;
+                    if (number.Index + number.Length < input[i].Length)
+                    {
+                        rightArea = input[i].Substring(number.Index + number.Length, 1);
+                        areas.Add(rightArea);
+                    }
+
+                    if(areas.Any(a => symbolRegex.Match(a).Success))
+                        partNumbers.Add(int.Parse(number.Value));
+
+                }
+            }
+
+
+            return partNumbers.Sum();
+        }
+
+        public override object Part2(List<string> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
