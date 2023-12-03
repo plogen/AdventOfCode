d9ee47b50afd9f0964984af2d1513edfe84e7371
plogen <plogen@gmail.com>
2023-12-03 16:35:41 +0100

    Created by using "aoc new day 1 -y 2023"

---
+++ aoc2023/Day/01/Day01.cs
@@ -0,0 +1,18 @@
+﻿using Common;
+
+//Puzzle: https://adventofcode.com/2023/day/1
+namespace aoc2023
+{
+    public class Day01: DayPuzzle
+    {
+        public override object Part1(List<string> input)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override object Part2(List<string> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
