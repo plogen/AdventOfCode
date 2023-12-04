d24d6076a0b563b7f0c2f06937397170c9cd048f
plogen <plogen@gmail.com>
2023-12-04 19:39:02 +0100

    Working Day04 Part1

---
+++ aoc2023.Tests/Day04Test.cs
@@ -0,0 +1,53 @@
+﻿using Common;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using aoc2023;
+
+namespace aoc2023.Tests
+{
+
+    [TestFixture]
+    public class Day04Test
+    {
+        private const int day = 04;
+        private List<string> input = null!;
+        private List<string> exampleInput = null!;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            input = ReadInputFile.GetInputDayPadding(04, "input.txt");
+            exampleInput = ReadInputFile.GetInputDayPadding(04, "exampleInput.txt");
+        }
+
+        [Test]
+        public void ExamplePart1()
+        {
+            var answer = new Day04().Part1(exampleInput);
+            Assert.AreEqual(13, answer);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var answer = new Day04().Part1(input);
+            Assert.AreEqual(27454, answer);
+        }
+
+        [Test]
+        public void ExamplePart2()
+        {
+            var answer = new Day04().Part2(exampleInput);
+            Assert.AreEqual(-1, answer);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            var answer = new Day04().Part2(input);
+            Assert.AreEqual(-1, answer);
+        }
+
+    }
+}
---
+++ aoc2023/Day/04/Day04.cs
@@ -0,0 +1,43 @@
+﻿using Common;
+using System.Text.RegularExpressions;
+
+//Puzzle: https://adventofcode.com/2023/day/4
+namespace aoc2023
+{
+    public class Day04: DayPuzzle
+    {
+        public static Regex cardNumberRegex = new Regex(@"Card\s+(\d+):");
+        public static Regex numbersRegex = new Regex(@"(\d+)");
+        public static Regex pickedNumbersRegex = new Regex(@".+\|(?:\s+(\d+))+");
+
+        public override object Part1(List<string> input)
+        {
+            var cards = input.Select(i => GetCard(i)).ToList();
+
+            double totalPoints = 0;
+            foreach (var card in cards)
+            {
+                int points = card.pickedNumbers.Select(p => card.winningNumbers.Contains(p) ? 1 : 0).Sum();
+                if(points > 0)
+                    totalPoints += Math.Pow(2, points - 1);
+            }
+            return totalPoints;
+        }
+
+        public override object Part2(List<string> input)
+        {
+            throw new NotImplementedException();
+        }
+
+        private static Card GetCard(string input)
+        {
+            var cardNumber = int.Parse(cardNumberRegex.Match(input).Groups[1].Value);
+            var parts = input.Split(':', '|');
+            var winningNumbers = numbersRegex.Matches(parts[1]).Select(n => int.Parse(n.Groups[1].Value)).ToList();
+            var pickedNumbers = numbersRegex.Matches(parts[2]).Select(n => int.Parse(n.Groups[1].Value)).ToList();
+            return new Card(cardNumber, winningNumbers, pickedNumbers);
+        }
+
+        private record Card(int cardNumber, List<int> winningNumbers, List<int> pickedNumbers);
+    }
+}
