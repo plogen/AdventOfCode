28199f6cd11b20698e32b9e834c4ab42042f75e9
plogen <plogen@gmail.com>
2023-12-06 21:55:37 +0100

    Working Day06 Part1

---
+++ aoc2023.Tests/Day06Test.cs
@@ -0,0 +1,53 @@
+﻿using Common;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using aoc2023;
+
+namespace aoc2023.Tests
+{
+
+    [TestFixture]
+    public class Day06Test
+    {
+        private const int day = 06;
+        private List<string> input = null!;
+        private List<string> exampleInput = null!;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            input = ReadInputFile.GetInputDayPadding(06, "input.txt");
+            exampleInput = ReadInputFile.GetInputDayPadding(06, "exampleInput.txt");
+        }
+
+        [Test]
+        public void ExamplePart1()
+        {
+            var answer = new Day06().Part1(exampleInput);
+            Assert.AreEqual(288, answer);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var answer = new Day06().Part1(input);
+            Assert.AreEqual(114400, answer);
+        }
+
+        [Test]
+        public void ExamplePart2()
+        {
+            var answer = new Day06().Part2(exampleInput);
+            Assert.AreEqual(-1, answer);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            var answer = new Day06().Part2(input);
+            Assert.AreEqual(-1, answer);
+        }
+
+    }
+}
---
+++ aoc2023/Day/06/Day06.cs
@@ -0,0 +1,47 @@
+﻿using Common;
+using System;
+using System.Text.RegularExpressions;
+
+//Puzzle: https://adventofcode.com/2023/day/6
+namespace aoc2023
+{
+    public class Day06 : DayPuzzle
+    {
+        public static Regex numbersRegex = new Regex(@"\s+(\d+)");
+
+        public override object Part1(List<string> input)
+        {
+            var raceTimes = numbersRegex.Matches(input[0]).Select(m => int.Parse(m.Groups[1].Value)).ToList();
+            var raceDistanceRecord = numbersRegex.Matches(input[1]).Select(m => int.Parse(m.Groups[1].Value)).ToList();
+            int[] wins = new int[raceTimes.Count];
+
+            for (int race = 0; race < raceTimes.Count; race++)
+            {
+                for (int tick = 0; tick < raceTimes[race]; tick++)
+                {
+                    var speed = tick;
+                    var timeLeft = raceTimes[race] - tick;
+                    var distance = speed * timeLeft;
+                    if (distance > raceDistanceRecord[race])
+                    {
+                        wins[race]++;
+                    }
+                }
+            }
+
+            int margin = 1;
+            foreach (int win in wins)
+            {
+                margin *= win;
+            }
+
+            return margin;
+        }
+
+
+        public override object Part2(List<string> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
