87c98e77267179691fca41dbde5fed2ce19d15cf
plogen <plogen@gmail.com>
2023-12-10 19:46:52 +0100

    Working Day08 Part1

---
+++ aoc2023.Tests/Day08Test.cs
@@ -0,0 +1,53 @@
+﻿using Common;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using aoc2023;
+
+namespace aoc2023.Tests
+{
+
+    [TestFixture]
+    public class Day08Test
+    {
+        private const int day = 08;
+        private List<string> input = null!;
+        private List<string> exampleInput = null!;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            input = ReadInputFile.GetInputDayPadding(08, "input.txt");
+            exampleInput = ReadInputFile.GetInputDayPadding(08, "exampleInput.txt");
+        }
+
+        [Test]
+        public void ExamplePart1()
+        {
+            var answer = new Day08().Part1(exampleInput);
+            Assert.AreEqual(6, answer);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var answer = new Day08().Part1(input);
+            Assert.AreEqual(-1, answer);
+        }
+
+        [Test]
+        public void ExamplePart2()
+        {
+            var answer = new Day08().Part2(exampleInput);
+            Assert.AreEqual(-1, answer);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            var answer = new Day08().Part2(input);
+            Assert.AreEqual(-1, answer);
+        }
+
+    }
+}
---
+++ aoc2023/Day/08/Day08.cs
@@ -0,0 +1,48 @@
+﻿using Common;
+using System.Text.RegularExpressions;
+
+//Puzzle: https://adventofcode.com/2023/day/8
+namespace aoc2023
+{
+    public class Day08: DayPuzzle
+    {
+        public static Regex nodesRegex = new Regex(@"\w+");
+
+        public override object Part1(List<string> input)
+        {
+            var instructions = input[0];
+
+            var nodes = input.Skip(2).Select(i => new Node(i.Substring(0,3), i.Substring(7,3), i.Substring(12,3))).ToList();
+
+            int i = 0;
+            string node = "AAA";
+            int stepps = 0;
+            while (true)
+            {
+                var currentNode = nodes.First(n => n.x == node);
+
+                if (currentNode.x == "ZZZ")
+                    break;
+
+                if (instructions[i] == 'L')
+                    node = currentNode.y;
+                else
+                    node = currentNode.z;
+
+                stepps++;
+                i++;
+                if (instructions.Count() == i)
+                    i = 0;
+            }
+
+            return stepps;
+        }
+
+        public override object Part2(List<string> input)
+        {
+            throw new NotImplementedException();
+        }
+
+        private record Node(string x, string y, string z);
+    }
+}
