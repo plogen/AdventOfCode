d86db12a6c9f6cf74307e9fd0e8d5a1c2c67d89e
plogen <plogen@gmail.com>
2023-12-17 16:29:43 +0100

    Working Day09 Part1

---
+++ aoc2023.Tests/Day09Test.cs
@@ -0,0 +1,54 @@
+﻿using Common;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using aoc2023;
+using System.Numerics;
+
+namespace aoc2023.Tests
+{
+
+    [TestFixture]
+    public class Day09Test
+    {
+        private const int day = 09;
+        private List<string> input = null!;
+        private List<string> exampleInput = null!;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            input = ReadInputFile.GetInputDayPadding(09, "input.txt");
+            exampleInput = ReadInputFile.GetInputDayPadding(09, "exampleInput.txt");
+        }
+
+        [Test]
+        public void ExamplePart1()
+        {
+            var answer = new Day09().Part1(exampleInput);
+            Assert.AreEqual(new BigInteger(114), answer);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var answer = new Day09().Part1(input);
+            Assert.AreEqual(new BigInteger(1939607039), answer);
+        }
+
+        [Test]
+        public void ExamplePart2()
+        {
+            var answer = new Day09().Part2(exampleInput);
+            Assert.AreEqual(-1, answer);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            var answer = new Day09().Part2(input);
+            Assert.AreEqual(-1, answer);
+        }
+
+    }
+}
---
+++ aoc2023/Day/09/Day09.cs
@@ -0,0 +1,75 @@
+﻿using Common;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Numerics;
+using System.Text.RegularExpressions;
+
+//Puzzle: https://adventofcode.com/2023/day/9
+namespace aoc2023
+{
+    public class Day09: DayPuzzle
+    {
+        public static Regex numbersRegex = new Regex(@"-?[0-9]\d*(\.\d+)?");
+        public override object Part1(List<string> input)
+        {
+
+            List<List<List<long>>> measurements = new();
+
+            foreach (string measurement in input)
+            {
+                List< List<long>> rows = new List< List<long>>();
+                var firstRound = numbersRegex.Matches(measurement).Select(x => long.Parse(x.Groups[0].Value)).ToList();
+                rows.Add(firstRound);
+
+                bool finnished = false;
+                int i = 0;
+                while (finnished == false)
+                {
+                    var newRow = GetNewList(rows[i]);
+                    rows.Add(newRow);
+                    i++;
+                    if(newRow.All(n => n == 0))
+                        finnished = true;
+                }
+
+
+                for (int r = rows.Count-1; r > 0; r--)
+                {
+                    if(r == rows.Count - 1)
+                        rows[r].Add(0);
+
+                    if (r > 0)
+                    {
+                        var rowAbove = rows[r - 1];
+                        var rowAboveCount = rowAbove.Count;
+                        var diff = rowAbove[rowAboveCount-1] + rows[r].Last();
+                         
+                        rows[r - 1].Add(diff);
+                    }
+                }
+
+                measurements.Add(rows);
+            }
+
+            var value = measurements.Select(x => new BigInteger(x.First().Last()));
+
+            return value.Aggregate(BigInteger.Add);
+        }
+
+        private static List<long> GetNewList(List<long> numbers)
+        {
+            List<long> steps = new List<long>();
+            for (int i = 0; i < numbers.Count - 1; i++)
+            {
+                steps.Add(numbers[i + 1] - numbers[i]);
+            }
+            return steps;
+        }
+
+        public override object Part2(List<string> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
