20b786b797116e93118a1f2db7f46041e4ab07f4
plogen <plogen@gmail.com>
2023-12-28 15:34:58 +0100

    Day12 template but no solution in place...

---
+++ aoc2023.Tests/Day12Test.cs
@@ -0,0 +1,53 @@
+﻿using Common;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using aoc2023;
+
+namespace aoc2023.Tests
+{
+
+    [TestFixture]
+    public class Day12Test
+    {
+        private const int day = 12;
+        private List<string> input = null!;
+        private List<string> exampleInput = null!;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            input = ReadInputFile.GetInputDayPadding(12, "input.txt");
+            exampleInput = ReadInputFile.GetInputDayPadding(12, "exampleInput.txt");
+        }
+
+        [Test]
+        public void ExamplePart1()
+        {
+            var answer = new Day12().Part1(exampleInput);
+            Assert.AreEqual(21, answer);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var answer = new Day12().Part1(input);
+            Assert.AreEqual(-1, answer);
+        }
+
+        [Test]
+        public void ExamplePart2()
+        {
+            var answer = new Day12().Part2(exampleInput);
+            Assert.AreEqual(-1, answer);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            var answer = new Day12().Part2(input);
+            Assert.AreEqual(-1, answer);
+        }
+
+    }
+}
---
+++ aoc2023/Day/12/Day12.cs
@@ -0,0 +1,43 @@
+﻿using Common;
+using System.Collections.Generic;
+
+//Puzzle: https://adventofcode.com/2023/day/12
+namespace aoc2023
+{
+    public class Day12: DayPuzzle
+    {
+        public override object Part1(List<string> input)
+        {
+            var springs = GetSprings(input);
+
+            return null;
+        }
+
+        private static List<Spring> GetSprings(List<string> rows)
+        {
+            List<Spring> springs = new();
+            foreach (var row in rows)
+            {
+                var data = row.Split(' ');
+
+                springs.Add(new()
+                {
+                    Line = data[0],
+                    Springs = data[1].Split(',').Select(x => int.Parse(x)).ToList()
+                });
+            }
+            return springs;
+        }
+
+        public override object Part2(List<string> input)
+        {
+            throw new NotImplementedException();
+        }
+
+        private class Spring
+        {
+            public string Line { get; set; }
+            public List<int> Springs { get; set; }
+        }
+    }
+}
