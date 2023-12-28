9d439794442a5812072622e71eafeaa2f1d8ffc8
plogen <plogen@gmail.com>
2023-12-28 15:36:07 +0100

    Day13 template

---
+++ aoc2023.Tests/Day13Test.cs
@@ -0,0 +1,53 @@
+﻿using Common;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using aoc2023;
+
+namespace aoc2023.Tests
+{
+
+    [TestFixture]
+    public class Day13Test
+    {
+        private const int day = 13;
+        private List<string> input = null!;
+        private List<string> exampleInput = null!;
+
+        [OneTimeSetUp]
+        public void Setup()
+        {
+            input = ReadInputFile.GetInputDayPadding(13, "input.txt");
+            exampleInput = ReadInputFile.GetInputDayPadding(13, "exampleInput.txt");
+        }
+
+        [Test]
+        public void ExamplePart1()
+        {
+            var answer = new Day13().Part1(exampleInput);
+            Assert.AreEqual(405, answer);
+        }
+
+        [Test]
+        public void Part1()
+        {
+            var answer = new Day13().Part1(input);
+            Assert.AreEqual(-1, answer);
+        }
+
+        [Test]
+        public void ExamplePart2()
+        {
+            var answer = new Day13().Part2(exampleInput);
+            Assert.AreEqual(-1, answer);
+        }
+
+        [Test]
+        public void Part2()
+        {
+            var answer = new Day13().Part2(input);
+            Assert.AreEqual(-1, answer);
+        }
+
+    }
+}
---
+++ aoc2023/Day/13/Day13.cs
@@ -0,0 +1,20 @@
+﻿using Common;
+
+//Puzzle: https://adventofcode.com/2023/day/13
+namespace aoc2023
+{
+    public class Day13: DayPuzzle
+    {
+        public override object Part1(List<string> input)
+        {
+            
+
+            return null;
+        }
+
+        public override object Part2(List<string> input)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
