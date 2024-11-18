79ecae6f737b7198b299c97f3ec80ffbb2b0d774
plogen <plogen@gmail.com>
2024-11-18 23:18:17 +0100

    Working Day1 Part1

---
+++ aoc2019.Tests/Day01Test.cs
@@ -0,0 +1,55 @@
+﻿using Common;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using aoc2019;
+
+namespace aoc2019.Tests;
+
+[TestFixture]
+public class Day01Test
+{
+    private const int day = 01;
+    private List<string> input = null!;
+    private List<string> exampleInput = null!;
+
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        input = ReadInputFile.GetInputDayPadding(01, "input.txt");
+        exampleInput = ReadInputFile.GetInputDayPadding(01, "exampleInput.txt");
+    }
+
+    [Test]
+    [TestCase(2, 12)]
+    [TestCase(2, 14)]
+    [TestCase(654, 1969)]
+    [TestCase(33583, 100756)]
+    public void ExamplePart1(int expected, int input)
+    {
+        var answer = new Day01().Part1(new List<string>{ input.ToString() });
+        Assert.That(expected, Is.EqualTo(answer));
+    }
+
+    [Test]
+    public void Part1()
+    {
+        var answer = new Day01().Part1(input);
+        Assert.That(answer, Is.EqualTo(-1));
+    }
+
+    [Test]
+    public void ExamplePart2()
+    {
+        var answer = new Day01().Part2(exampleInput);
+        Assert.That(-1, Is.EqualTo(answer));
+    }
+
+    [Test]
+    public void Part2()
+    {
+        var answer = new Day01().Part2(input);
+        Assert.That(-1, Is.EqualTo(answer));
+    }
+
+}
---
+++ aoc2019/Day/01/Day01.cs
@@ -0,0 +1,17 @@
+﻿using Common;
+
+//Puzzle: https://adventofcode.com/2019/day/1
+namespace aoc2019;
+public class Day01: DayPuzzle
+{
+    public override object Part1(List<string> input)
+    {
+        return input.Select(Int32.Parse)
+            .Sum(m => (int)Math.Floor(m / 3d) - 2);
+    }
+
+    public override object Part2(List<string> input)
+    {
+        throw new NotImplementedException();
+    }
+}
