e2b83818152dc1bb96522c07855433fad0cb0727
plogen <plogen@gmail.com>
2024-11-18 22:31:42 +0100

    New Assert syntax.

--- Common.Tests/HelperTest.cs
+++ Common.Tests/HelperTest.cs
@@ -11,29 +11,29 @@ namespace Common.Tests
         [Test]
         public void OperatorAddTest()
         {
-            Assert.AreEqual(3, Helper.Operators['+'](1, 2));
-            Assert.AreEqual(5, Helper.Operators['+'](3, 2));
+            Assert.That(3 == Helper.Operators['+'](1, 2));
+            Assert.That(5 == Helper.Operators['+'](3, 2));
         }
 
         [Test]
         public void OperatorSubtractTest()
         {
-            Assert.AreEqual(-1, Helper.Operators['-'](1, 2));
-            Assert.AreEqual(1, Helper.Operators['-'](3, 2));
+            Assert.That(-1 == Helper.Operators['-'](1, 2));
+            Assert.That(1 == Helper.Operators['-'](3, 2));
         }
 
         [Test]
         public void OperatorDevideTest()
         {
-            Assert.AreEqual(1, Helper.Operators['/'](2, 2));
-            Assert.AreEqual(3, Helper.Operators['/'](6, 2));
+            Assert.That(1 == Helper.Operators['/'](2, 2));
+            Assert.That(3 == Helper.Operators['/'](6, 2));
         }
 
         [Test]
         public void OperatorMultiplyTest()
         {
-            Assert.AreEqual(4, Helper.Operators['*'](2, 2));
-            Assert.AreEqual(12, Helper.Operators['*'](6, 2));
+            Assert.That(4 == Helper.Operators['*'](2, 2));
+            Assert.That(12 == Helper.Operators['*'](6, 2));
         }
     }
 }
