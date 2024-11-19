d8ac28951cf2e7be0689718bcdd16903df6dad01
plogen <plogen@gmail.com>
2024-11-19 22:25:37 +0100

    Working Day4 Part1

---
+++ aoc2019.Tests/Day04Test.cs
@@ -0,0 +1,44 @@
+﻿using Common;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using aoc2019;
+
+namespace aoc2019.Tests;
+
+[TestFixture]
+public class Day04Test
+{
+    private const int day = 04;
+    private List<string> input = null!;
+    private List<string> exampleInput = null!;
+
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        input = ReadInputFile.GetInputDayPadding(04, "input.txt");
+        exampleInput = ReadInputFile.GetInputDayPadding(04, "exampleInput.txt");
+    }
+
+    [Test]
+    public void Part1()
+    {
+        var answer = new Day04().Part1(input);
+        Assert.That(answer, Is.EqualTo(1767));
+    }
+
+    [Test]
+    public void ExamplePart2()
+    {
+        var answer = new Day04().Part2(exampleInput);
+        Assert.That(answer, Is.EqualTo(-1));
+    }
+
+    [Test]
+    public void Part2()
+    {
+        var answer = new Day04().Part2(input);
+        Assert.That(answer, Is.EqualTo(-1));
+    }
+
+}
---
+++ aoc2019/Day/04/Day04.cs
@@ -0,0 +1,69 @@
+﻿using Common;
+
+//Puzzle: https://adventofcode.com/2019/day/4
+namespace aoc2019;
+public class Day04: DayPuzzle
+{
+    public override object Part1(List<string> input)
+    {
+        var range = input[0].Split('-').Select(Int32.Parse);
+        
+        var size = range.Last() - range.First();
+
+        int passwordCount = 0;
+        var value = range.First();
+        for (int i = 0; i < size; i++)
+        {
+            if (Increasing(value) && HasDouble(value))
+                passwordCount++;
+
+            value++;
+        }
+
+        return passwordCount;
+    }
+
+    private bool Increasing(int value)
+    {
+        var array = GetIntArray(value);
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            if (array[i] > array[i+1])
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private bool HasDouble(int value)
+    {
+        var array = GetIntArray(value);
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            if (array[i] == array[i + 1])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public override object Part2(List<string> input)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    int[] GetIntArray(int num)
+    {
+        List<int> listOfInts = new List<int>();
+        while (num > 0)
+        {
+            listOfInts.Add(num % 10);
+            num = num / 10;
+        }
+        listOfInts.Reverse();
+        return listOfInts.ToArray();
+    }
+}
