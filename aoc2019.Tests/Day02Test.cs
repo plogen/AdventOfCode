177bb3e3391b54560226a8f7a14f5dad04ce9b88
plogen <plogen@gmail.com>
2024-11-19 18:13:11 +0100

    Working Day2 Part1

---
+++ aoc2019.Tests/Day02Test.cs
@@ -0,0 +1,51 @@
+﻿using Common;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using aoc2019;
+
+namespace aoc2019.Tests;
+
+[TestFixture]
+public class Day02Test
+{
+    private const int day = 02;
+    private List<string> input = null!;
+    private List<string> exampleInput = null!;
+
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        input = ReadInputFile.GetInputDayPadding(02, "input.txt");
+        exampleInput = ReadInputFile.GetInputDayPadding(02, "exampleInput.txt");
+    }
+
+    [Test]
+    public void ExamplePart1()
+    {
+        var answer = new Day02().Part1(exampleInput);
+        Assert.That(answer, Is.EqualTo(-1));
+    }
+
+    [Test]
+    public void Part1()
+    {
+        var answer = new Day02().Part1(input);
+        Assert.That(answer, Is.EqualTo(4090689));
+    }
+
+    [Test]
+    public void ExamplePart2()
+    {
+        var answer = new Day02().Part2(exampleInput);
+        Assert.That(answer, Is.EqualTo(-1));
+    }
+
+    [Test]
+    public void Part2()
+    {
+        var answer = new Day02().Part2(input);
+        Assert.That(answer, Is.EqualTo(-1));
+    }
+
+}
---
+++ aoc2019/Day/02/Day02.cs
@@ -0,0 +1,56 @@
+﻿using Common;
+
+//Puzzle: https://adventofcode.com/2019/day/2
+namespace aoc2019;
+public class Day02: DayPuzzle
+{
+    public override object Part1(List<string> input)
+    {
+        var ints = input.First().Split(',').Select(Int32.Parse).ToList();
+
+        //reproduce 1202 program alarm
+        ints[1] = 12;
+        ints[2] = 2;
+
+        bool terminated = false;
+        int index = 0;
+        while(!terminated)
+        {
+            terminated = Calculator(ints, index);
+            index += 4;
+        }
+
+        return ints[0];
+        
+    }
+
+    public override object Part2(List<string> input)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    private bool Calculator(List<int> ints, int index)
+    {
+        var opcode = ints[index];
+        if (opcode == 99)
+        {
+            return true;
+        }
+        if (opcode == 1)
+        {
+            var value = ints[ints[index + 1]] + ints[ints[index + 2]];
+            ints[ints[index + 3]] = value;
+            return false;
+        }
+        if (opcode == 2)
+        {
+            var value = ints[ints[index + 1]] * ints[ints[index + 2]];
+            ints[ints[index + 3]] = value;
+            return false;
+        }
+
+        throw new InvalidOperationException("Unhandled opcode");
+    }
+
+}
