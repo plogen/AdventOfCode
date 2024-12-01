0131ddde5b76ef87dea719e8a37bbccd14555945
plogen <plogen@gmail.com>
2024-12-01 11:21:11 +0100

    Working Day1 Part1

---
+++ aoc2024.Tests/Day01Test.cs
@@ -0,0 +1,51 @@
+﻿using Common;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using aoc2024;
+
+namespace aoc2024.Tests;
+
+[TestFixture]
+public class Day01Test
+{
+    private const int day = 01;
+    private List<string> input = null!;
+    private List<string> exampleInput = null!;
+
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        input = ReadInputFile.GetInputDayPadding(01, "input.txt");
+        exampleInput = ReadInputFile.GetInputDayPadding(01, "exampleInput.txt");
+    }
+
+    [Test]
+    public void ExamplePart1()
+    {
+        var answer = new Day01().Part1(exampleInput);
+        Assert.That(answer, Is.EqualTo(11));
+    }
+
+    [Test]
+    public void Part1()
+    {
+        var answer = new Day01().Part1(input);
+        Assert.That(answer, Is.EqualTo(1579939));
+    }
+
+    [Test]
+    public void ExamplePart2()
+    {
+        var answer = new Day01().Part2(exampleInput);
+        Assert.That(answer, Is.EqualTo(-1));
+    }
+
+    [Test]
+    public void Part2()
+    {
+        var answer = new Day01().Part2(input);
+        Assert.That(answer, Is.EqualTo(-1));
+    }
+
+}
---
+++ aoc2024/Day/01/Day01.cs
@@ -0,0 +1,51 @@
+﻿using Common;
+using System.Text;
+using System.Text.RegularExpressions;
+
+//Puzzle: https://adventofcode.com/2024/day/1
+namespace aoc2024;
+public class Day01: DayPuzzle
+{
+    public override object Part1(List<string> input)
+    {
+        var list = GetList(input);
+        var totalDistance = GetTotalDistance(list);
+        return totalDistance;
+    }
+
+    public override object Part2(List<string> input)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    private ListInput GetList(List<string> input)
+    {
+        List<int> left = new List<int>();
+        List<int> right = new List<int>();
+        foreach (var row in input)
+        {
+            var values = row.Split([' '], StringSplitOptions.RemoveEmptyEntries);
+            left.Add(int.Parse(values[0]));
+            right.Add(int.Parse(values[1]));
+        }
+        return new ListInput(left, right);
+    }
+
+    private long GetTotalDistance(ListInput listInput)
+    {
+        long totalDistance = 0;
+        listInput.left.Sort();
+        listInput.right.Sort();
+
+        for (int i = 0; i < listInput.left.Count; i++)
+        {
+            totalDistance += Math.Abs(listInput.right[i] - listInput.left[i]);
+        }
+
+        return totalDistance;
+    }
+
+    record ListInput(List<int> left, List<int> right);
+
+}
