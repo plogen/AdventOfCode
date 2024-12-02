752507a2c466c7dc9d2e363ddc7611ae57973b2e
plogen <plogen@gmail.com>
2024-12-02 20:53:29 +0100

    Working Day2 Part1

---
+++ aoc2024/Day/02/Day02.cs
@@ -0,0 +1,47 @@
+﻿using Common;
+
+//Puzzle: https://adventofcode.com/2024/day/2
+namespace aoc2024;
+public class Day02: DayPuzzle
+{
+    public override object Part1(List<string> input)
+    {
+        var reports = GetReports(input);
+
+        foreach (var report in reports)
+        {
+            var isSafe = IsSafe(report);
+        }
+
+        return reports.Where(r => IsSafe(r)).Count();
+    }
+
+    public override object Part2(List<string> input)
+    {
+        throw new NotImplementedException();
+    }
+
+    private List<int[]> GetReports(List<string> input)
+    {
+        var reports = new List<int[]>();
+
+        foreach (var row in input)
+        {
+            reports.Add(row.Split(' ').Select(int.Parse).ToArray());
+        }
+
+        return reports;
+    }
+
+    private bool IsSafe(int[] report)
+    {
+        if(!report.IsAllIncreasing() && !report.IsAllDecreasing())
+            return false;
+
+        if(!report.IsWithinSteps(3))
+            return false;
+
+        return true;
+    }
+
+}
