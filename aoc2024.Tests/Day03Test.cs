8d237893988b1e36ba393b556ee50e8112e4b720
plogen <plogen@gmail.com>
2024-12-03 20:19:00 +0100

    Working Day3 Part1

---
+++ aoc2024.Tests/Day03Test.cs
@@ -0,0 +1,51 @@
+﻿using Common;
+using NUnit.Framework;
+using System.Collections.Generic;
+using System.Linq;
+using aoc2024;
+
+namespace aoc2024.Tests;
+
+[TestFixture]
+public class Day03Test
+{
+    private const int day = 03;
+    private List<string> input = null!;
+    private List<string> exampleInput = null!;
+
+    [OneTimeSetUp]
+    public void Setup()
+    {
+        input = ReadInputFile.GetInputDayPadding(03, "input.txt");
+        exampleInput = ReadInputFile.GetInputDayPadding(03, "exampleInput.txt");
+    }
+
+    [Test]
+    public void ExamplePart1()
+    {
+        var answer = new Day03().Part1(exampleInput);
+        Assert.That(answer, Is.EqualTo(161));
+    }
+
+    [Test]
+    public void Part1()
+    {
+        var answer = new Day03().Part1(input);
+        Assert.That(answer, Is.EqualTo(173529487));
+    }
+
+    [Test]
+    public void ExamplePart2()
+    {
+        var answer = new Day03().Part2(exampleInput);
+        Assert.That(answer, Is.EqualTo(-1));
+    }
+
+    [Test]
+    public void Part2()
+    {
+        var answer = new Day03().Part2(input);
+        Assert.That(answer, Is.EqualTo(-1));
+    }
+
+}
---
+++ aoc2024/Day/03/Day03.cs
@@ -0,0 +1,40 @@
+﻿using Common;
+using System.Text.RegularExpressions;
+
+//Puzzle: https://adventofcode.com/2024/day/3
+namespace aoc2024;
+public class Day03: DayPuzzle
+{
+    public override object Part1(List<string> input)
+    {
+        var numbers = GetNumbers(input);
+
+        return GetResult(numbers);
+    }
+
+    public override object Part2(List<string> input)
+    {
+        throw new NotImplementedException();
+    }
+
+
+    private long GetResult(List<Tuple<int, int>> numbers)
+    {
+        return numbers.Select(x => x.Item1 * x.Item2).Sum();
+    }
+
+    private List<Tuple<int, int>> GetNumbers(List<string> input)
+    {
+        var list = new List<Tuple<int, int>>();
+        foreach (var row in input)
+        {
+            list.AddRange(Regex.Matches(row, @"mul\((\d{1,4}),(\d{1,4})\)")
+                .Select(x => new Tuple<int, int>( int.Parse(x.Groups[1].Value), int.Parse(x.Groups[2].Value)))
+                .ToList());
+
+        }
+        return list;
+    }
+
+
+}
